59bb49a648a81064736d6df807e1cc559d2e6c85
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-12-12 01:03:55 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,21 +8,21 @@ if (navigator.serviceWorker) {
   })
 }
 
-"use strict"
+;("use strict")
 
 function calculate() {
-  const number1 = parseInt(document.getElementById("pay").value);
-  const number2 = parseInt(document.getElementById("pay2").value);
-  var age = 0;
-  var answer = number1;
-  
+  const number1 = parseInt(document.getElementById("pay").value)
+  const number2 = parseInt(document.getElementById("pay2").value)
+  var age = 0
+  var answer = number1
+
   if (number1 > 0 && number2 > 0) {
     while (answer >= number2) {
-      age = age + 1;
-      answer = answer - number2;
+      age = age + 1
+      answer = answer - number2
     }
   }
 
   document.getElementById("answers").innerHTML =
-    number1 +  " ÷ " + number2 + " = " + age;
+    number1 + " ÷ " + number2 + " = " + age
 }
